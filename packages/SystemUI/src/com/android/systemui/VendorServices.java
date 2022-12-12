bf1a31a6ad6588358f97605582ca644f4f3a17e2
Matt Filetto <matt.filetto@gmail.com>
2022-12-12 12:30:41 -0800

    NowPlaying 2

--- packages/SystemUI/src/com/android/systemui/VendorServices.java
+++ packages/SystemUI/src/com/android/systemui/VendorServices.java
@@ -16,20 +16,65 @@
 
 package com.android.systemui;
 
+import android.app.AlarmManager;
 import android.content.Context;
+
+import com.android.systemui.Dumpable;
+import com.android.systemui.R;
+import com.android.systemui.VendorServices;
+import com.android.systemui.assist.AssistManager;
+import com.android.systemui.dagger.SysUISingleton;
+import com.android.systemui.flags.FeatureFlags;
+import com.android.systemui.statusbar.phone.CentralSurfaces;
+import com.android.systemui.statusbar.policy.FlashlightController;
+
+import com.android.systemui.ambient.AmbientIndicationContainer;
+import com.android.systemui.ambient.AmbientIndicationService;
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import dagger.Lazy;
+
 
 /**
  * Placeholder for any vendor-specific services.
  */
-public class VendorServices extends CoreStartable {
-
-    public VendorServices(Context context) {
+public class VendorServices extends CoreStartable { 
+    private final ArrayList<Object> mServices = new ArrayList<>();
+    private final AlarmManager mAlarmManager;
+    private final AssistManager mAssistManager;
+    private final CentralSurfaces mCentralSurfaces;
+    
+        @Inject
+    public VendorServices(Context context, AlarmManager alarmManager, AssistManager assistManager, CentralSurfaces centralSurfaces) {
         super(context);
+        mAlarmManager = alarmManager;
+        mAssistManager = assistManager;
+        mCentralSurfaces = centralSurfaces;
     }
 
     @Override
     public void start() {
-        // no-op
+        AmbientIndicationContainer ambientIndicationContainer = (AmbientIndicationContainer) mCentralSurfaces.getNotificationShadeWindowView().findViewById(R.id.ambient_indication_container);
+        ambientIndicationContainer.initializeView(mCentralSurfaces);
+        addService(new AmbientIndicationService(mContext, ambientIndicationContainer, mAlarmManager));
+    }
+    
+        @Override
+    public void dump(PrintWriter printWriter, String[] strArr) {
+        for (int i = 0; i < mServices.size(); i++) {
+            if (mServices.get(i) instanceof Dumpable) {
+                ((Dumpable) mServices.get(i)).dump(printWriter, strArr);
+            }
+        }
     }
 
+    private void addService(Object obj) {
+        if (obj != null) {
+            mServices.add(obj);
+        }
+    }
 }
